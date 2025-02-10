a12710eb25ebe96fb7267fdc8ec4d18e5533f581
julien <julienchauvin@hotmail.fr>
2025-02-10 15:46:58 +0100

    dev-footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
 import { ArtisanComponent } from "./pages/artisan/artisan.component";
+import { FooterComponent } from './component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ArtisanComponent],
+  imports: [RouterOutlet, HeaderComponent, ArtisanComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
