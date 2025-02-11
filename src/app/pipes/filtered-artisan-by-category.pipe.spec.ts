dd523a117f762ca3884232a4c6fec0a60ca49b3c
julien <julienchauvin@hotmail.fr>
2025-02-11 16:44:06 +0100

    création du pipe filteredArtisanByCategory + artisanList

---
+++ src/app/pipes/filtered-artisan-by-category.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilteredArtisanByCategoryPipe } from './filtered-artisan-by-category.pipe';
+
+describe('FilteredArtisanByCategoryPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilteredArtisanByCategoryPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
