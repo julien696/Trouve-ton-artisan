1544998ee1fe3c7ca73914fefd1fd4c4b11ef744
julien <julienchauvin@hotmail.fr>
2025-02-11 16:19:04 +0100

    dev-router + start dev-artisanList

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
